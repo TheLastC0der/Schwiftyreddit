56db0fa855dac7ad3c4f5fbe9ffb75a07d3fe684
TheLastC0der <30476750+thelastpringlez@users.noreply.github.com>
2018-01-13 04:06:36 -0500

    big update

--- Schwiftology/config/routes.rb
+++ Schwiftology/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/links', to: 'links#index'
+  get '/new', to: 'links#new'
+  get '/about', to: 'pages#about'
+  
   resources :links
 
   root to: "pages#home"
-  get '/links', to: 'links#index'
-  get '/new', to: 'links#new'
-  
-  get '/about', to: 'pages#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
